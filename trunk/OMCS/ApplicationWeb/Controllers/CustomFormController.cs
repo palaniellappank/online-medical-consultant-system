5a780d0516f106ab5f980746159823ef720c914b
trannguyentiensu <trannguyentiensu@fd546a31-7132-cf25-b24b-de122df746d0>
2014-05-28 15:01:25 +0000

    Delete unnecessary files

---
+++ trunk/OMCS/ApplicationWeb/Controllers/CustomFormController.cs
@@ -0,0 +1,17 @@
+﻿using Security.DAL.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Security.Controllers
+{
+    public class CustomFormController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
