8ae81e13684b6dd2ef58153681bf26cf335efe37
trannguyentiensu <trannguyentiensu@fd546a31-7132-cf25-b24b-de122df746d0>
2014-06-05 14:32:50 +0000

    Add BusinessLogic demo

---
+++ OMCS/ApplicationBLL/BusinessLogic/MedicalProfileTemplateBusinessLogic.cs
@@ -0,0 +1,101 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using OMCS.DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMCS.BLL
+{
+    public class MedicalProfileTemplateBusinessLogic
+    {
+        private readonly OMCSDBContext db = new OMCSDBContext();
+
+        public string ShowTemplate(int id)
+        {
+            var listCustomSnippets = db.CustomSnippets.Where(s => s.MedicalProfileTemplateId == id).ToList();
+            dynamic result = new JArray();
+            foreach (var customSnippet in listCustomSnippets)
+            {
+                dynamic snippet = new JObject();
+                snippet.title = customSnippet.Title;
+                snippet.fields = new JObject() as dynamic;
+                var listCustomSnippetFields = from snippetField in db.CustomSnippetFields
+                                              where snippetField.CustomSnippetId == customSnippet.CustomSnippetId
+                                              select snippetField;
+                foreach (var customSnippetField in listCustomSnippetFields)
+                {
+                    dynamic metadata = new JObject();
+                    dynamic value;
+                    if (customSnippetField.Value.Contains("[") && customSnippetField.Value.Contains("]"))
+                    {
+                        value = JArray.Parse(customSnippetField.Value);
+                    }
+                    else
+                    {
+                        value = customSnippetField.Value;
+                    }
+                    metadata.label = customSnippetField.Label;
+                    metadata.type = customSnippetField.Type;
+                    metadata.value = value;
+                    metadata.name = customSnippetField.Name;
+                    snippet.fields.Add(customSnippetField.FieldName, metadata);
+                }
+                result.Add(snippet);
+            }
+            string json = JsonConvert.SerializeObject(result, Formatting.None);
+            return json;
+        }
+
+        public void SaveTemplate(string jsonString, int id)
+        {
+            var listCustomSnippets = db.CustomSnippets.Where(s => s.MedicalProfileTemplateId == id);
+            foreach (var entity in listCustomSnippets)
+            {
+                db.CustomSnippets.Remove(entity);
+            }
+            db.SaveChanges();
+
+            JArray listSnippets = JArray.Parse(jsonString) as JArray;
+
+            foreach (dynamic snippet in listSnippets)
+            {
+                CustomSnippet customSnippet = new CustomSnippet { Title = snippet.title, MedicalProfileTemplateId = id };
+                db.CustomSnippets.Add(customSnippet);
+                db.SaveChanges();
+                customSnippet.CustomSnippetFields = new Collection<CustomSnippetField>();
+                if (snippet.fields != null)
+                {
+                    foreach (dynamic snippetField in snippet.fields)
+                    {
+                        //string str = ((object)snippetField).ToString();
+                        //Debug.WriteLine("1" + str);
+                        dynamic metadata = snippetField.Value;
+
+                        if ("id".Equals(snippetField.Name))
+                        {
+                            metadata.value = customSnippet.CustomSnippetId;
+                        }
+                        //str = ((object)metadata).ToString();
+                        CustomSnippetField customSnippetField = new CustomSnippetField
+                        {
+                            CustomSnippet = customSnippet,
+                            FieldName = snippetField.Name,
+                            Label = metadata.label,
+                            Type = metadata.type,
+                            Value = ((object)metadata.value).ToString(),
+                            Name = metadata.name
+                        };
+                        customSnippet.CustomSnippetFields.Add(customSnippetField);
+                    }
+                }
+                db.Entry(customSnippet).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ OMCS/ApplicationWeb/Controllers/ImmunizationController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OMCS.DAL.Model;
+
+namespace MvcApplication1.Controllers
+{
+    public class ImmunizationController : Controller
+    {
+        private OMCSDBContext db = new OMCSDBContext();
+
+        //
+        // GET: /Immunization/
+
+        public ActionResult Index()
+        {
+            var immunizations = db.Immunizations.Include(i => i.MedicalProfile);
+            return View(immunizations.ToList());
+        }
+
+        //
+        // GET: /Immunization/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Immunization immunization = db.Immunizations.Find(id);
+            if (immunization == null)
+            {
+                return HttpNotFound();
+            }
+            return View(immunization);
+        }
+
+        //
+        // GET: /Immunization/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.MedicalProfileId = new SelectList(db.MedicalProfiles, "MedicalProfileId", "HeartRate");
+            return View();
+        }
+
+        //
+        // POST: /Immunization/Create
+
+        [HttpPost]
+        public ActionResult Create(Immunization immunization)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Immunizations.Add(immunization);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MedicalProfileId = new SelectList(db.MedicalProfiles, "MedicalProfileId", "HeartRate", immunization.MedicalProfileId);
+            return View(immunization);
+        }
+
+        //
+        // GET: /Immunization/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Immunization immunization = db.Immunizations.Find(id);
+            if (immunization == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MedicalProfileId = new SelectList(db.MedicalProfiles, "MedicalProfileId", "HeartRate", immunization.MedicalProfileId);
+            return View(immunization);
+        }
+
+        //
+        // POST: /Immunization/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Immunization immunization)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(immunization).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MedicalProfileId = new SelectList(db.MedicalProfiles, "MedicalProfileId", "HeartRate", immunization.MedicalProfileId);
+            return View(immunization);
+        }
+
+        //
+        // GET: /Immunization/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Immunization immunization = db.Immunizations.Find(id);
+            if (immunization == null)
+            {
+                return HttpNotFound();
+            }
+            return View(immunization);
+        }
+
+        //
+        // POST: /Immunization/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Immunization immunization = db.Immunizations.Find(id);
+            db.Immunizations.Remove(immunization);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
