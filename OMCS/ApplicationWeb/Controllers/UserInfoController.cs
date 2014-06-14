844c8a95dd7ba3a6b09d6270b0e98e6bbfc4facb
trannguyentiensu <trannguyentiensu@fd546a31-7132-cf25-b24b-de122df746d0>
2014-06-14 12:16:21 +0000

    [Patient update Personal Health Record]

--- OMCS/ApplicationWeb/Controllers/UserInfoController.cs
+++ OMCS/ApplicationWeb/Controllers/UserInfoController.cs
@@ -1,7 +1,13 @@
-﻿using Security.DAL.Security;
+﻿using Newtonsoft.Json.Linq;
+using OMCS.DAL.Model;
+using Security.DAL.Security;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,7 +18,29 @@ namespace Security.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            Debug.WriteLine(User.UserId);
+            var patient = _db.Patients.Where(pa => pa.UserId == User.UserId).SingleOrDefault();
+            return View(patient);
+        }
+
+        public JObject Save(string name, string value, int pk)
+        {
+            var patient = _db.Patients.Where(pa => pa.UserId == User.UserId).SingleOrDefault();
+            Type type = typeof(Patient);
+            PropertyInfo pi = type.GetProperty(name, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+            if ("birthday".Equals(name))
+            {
+                DateTime datetime = DateTime.ParseExact(value, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+                pi.SetValue(patient, datetime);
+            } else
+            {
+                pi.SetValue(patient, Convert.ChangeType(value, pi.PropertyType), null);
+            }
+            _db.SaveChanges();
+            JObject result = new JObject();
+            result.Add("status", "error");
+            result.Add("msg", "huhu");
+            return result;
         }
     }
 }
