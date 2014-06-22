df6f08606cffa07e75c118c8314edfe6a2c24d98
danhtcse60705@fpt.edu.vn <danhtcse60705@fpt.edu.vn@fd546a31-7132-cf25-b24b-de122df746d0>
2014-06-22 13:14:49 +0000

    [Finish Chatting]

--- trunk/OMCS/ApplicationWeb/Controllers/UserConversationController.cs
+++ trunk/OMCS/ApplicationWeb/Controllers/UserConversationController.cs
@@ -1,17 +1,46 @@
-﻿using Security.DAL.Security;
+﻿using OMCS.DAL.Model;
+using Security.DAL.Security;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace OMCS.Web.Controllers
+namespace Security.Controllers
 {
-     [CustomAuthorize(Roles= "User")]
+    [CustomAuthorize(Roles = "User, Doctor")]
     public class UserConversationController : BaseController
     {
+        OMCSDBContext db = new OMCSDBContext();
         public ActionResult Index()
         {
+            var user = db.Users.Where(u => u.UserId == User.UserId).SingleOrDefault();
+            ViewBag.Name = user.FullName;
+            ViewBag.Username = user.Username;           
+            return View();
+        }
+
+        public ActionResult Chat()
+        {
+            //var user = db.Users.Where(u => u.UserId == User.UserId).SingleOrDefault();
+            //ViewBag.Name = user.FullName;
+            //Debug.WriteLine(user.FullName);
+            //ViewBag.Username = user.Username;
+            return View();
+        }
+
+        public ActionResult ShowConversation()
+        {
+            return View();
+        }
+
+        public ActionResult ChatRoom()
+        {
+            //var user = db.Users.Where(u => u.UserId == User.UserId).SingleOrDefault();
+            //ViewBag.Name = user.FullName;
+            //ViewBag.Username = user.Username;          
             return View();
         }
     }
---
+++ trunk/OMCS/ApplicationWeb/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(MvcApplication1.Startup))]
+namespace MvcApplication1
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
