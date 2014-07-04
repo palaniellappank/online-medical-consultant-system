447344bf5a0a6e2d6f756374017af33873a0fc55
trannguyentiensu <trannguyentiensu@fd546a31-7132-cf25-b24b-de122df746d0>
2014-07-04 16:49:21 +0000

    [Doctor create medical profile for patient] Manage FilmDocument

---
+++ OMCS/ApplicationWeb/Controllers/AllergyController.cs
@@ -0,0 +1,76 @@
+﻿using OMCS.BLL;
+using OMCS.DAL.Model;
+using Security.DAL.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PagedList;
+using System.Web.Mvc;
+using OMCS.Web.DTO;
+using System.Diagnostics;
+using Newtonsoft.Json.Linq;
+using System.Data;
+using System.IO;
+
+namespace OMCS.Web.Controllers
+{
+    public class AllergyController : BaseController
+    {
+        public ActionResult List(int medicalProfileId)
+        {
+            var allergies = _db.Allergies.Where(
+                x => (x.MedicalProfileId == medicalProfileId))
+                .OrderByDescending(x => x.AllergyTypeId)
+                .OrderByDescending(x => x.DateLastOccurred)
+                .ToList();
+            return PartialView("_List", allergies);
+        }
+
+        public ActionResult Create(int medicalProfileId)
+        {
+            var allergy = new Allergy
+            {
+                MedicalProfileId = medicalProfileId
+            };
+            return PartialView("_Create", allergy);
+        }
+
+        [HttpPost]
+        public JObject Create(Allergy allergy)
+        {
+            _db.Allergies.Add(allergy);
+            _db.SaveChanges();
+            dynamic result = new JObject();
+            result.result = "ok";
+            return result;
+        }
+
+        public ActionResult Edit(int id = 0)
+        {
+            Allergy allergy = _db.Allergies.Find(id);
+            return PartialView("_Edit", allergy);
+        }
+
+        [HttpPost]
+        public JObject Edit(Allergy allergy)
+        {
+            _db.Entry(allergy).State = EntityState.Modified;
+            _db.SaveChanges();
+            dynamic result = new JObject();
+            result.result = "ok";
+            return result;
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public JObject DeleteConfirmed(int id)
+        {
+            Allergy allergy = _db.Allergies.Find(id);
+            _db.Allergies.Remove(allergy);
+            _db.SaveChanges();
+            dynamic result = new JObject();
+            result.result = "ok";
+            return result;
+        }
+    }
+}
