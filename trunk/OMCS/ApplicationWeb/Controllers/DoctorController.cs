08e98c19f8e9d67766996f6eaf584e91855022c8
danhtcse60705@fpt.edu.vn <danhtcse60705@fpt.edu.vn@fd546a31-7132-cf25-b24b-de122df746d0>
2014-09-05 15:43:21 +0000

    Fix count message in doctor page

--- trunk/OMCS/ApplicationWeb/Controllers/DoctorController.cs
+++ trunk/OMCS/ApplicationWeb/Controllers/DoctorController.cs
@@ -17,7 +17,8 @@ namespace OMCS.Web.Controllers
             ViewBag.recentComments = _db.Comments.Where(
                 x => x.DoctorId == User.UserId).Take(5).OrderByDescending(x => x.PostedDate).ToList();
             ViewBag.recentMessage = _db.Conversations.Where(c => c.DoctorId == User.UserId).OrderByDescending(x => x.DateConsulted).ToList();
-            ViewBag.countMessage = _db.Conversations.Where(c => c.IsDoctorRead == false).Count();
+            ViewBag.countMessage = _db.Conversations.Where(c => (c.IsDoctorRead == false) && (c.DoctorId == User.UserId)).Count();
+            ViewBag.doctorId = _db.Doctors.Where(d => d.UserId == User.UserId).SingleOrDefault();           
             return View();
         }
 
