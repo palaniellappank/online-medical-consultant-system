8c24a3b904e859013d6e3683aa95014270e1b6bf
danhtcse60705@fpt.edu.vn <danhtcse60705@fpt.edu.vn@fd546a31-7132-cf25-b24b-de122df746d0>
2014-09-12 01:01:11 +0000

    [Patient add/update medical records]

---
+++ trunk/OMCS/ApplicationWeb/Controllers/TreatmentHistoryPatientController.cs
@@ -0,0 +1,54 @@
+﻿using OMCS.BLL;
+using OMCS.DAL.Model;
+using Security.DAL.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PagedList;
+using System.Web.Mvc;
+using OMCS.Web.DTO;
+using System.Diagnostics;
+using Newtonsoft.Json.Linq;
+using System.Data;
+
+namespace OMCS.Web.Controllers
+{
+    public class TreatmentHistoryPatientController : BaseController
+    {
+        private AdminUserBusiness business = new AdminUserBusiness();
+        private MedicalProfileBusiness medicalProfileBusiness = new MedicalProfileBusiness();
+
+        public ActionResult List(int medicalProfileId)
+        {
+            var treatments = _db.TreatmentHistories.Where(
+                x => x.MedicalProfileId == medicalProfileId)
+                .OrderByDescending(x => x.DateCreated)
+                .ToList();
+            return PartialView("List", treatments);
+        }
+
+        public ActionResult Create(int medicalProfileId)
+        {
+            var doctor = _db.Doctors.ToList();
+            ViewBag.Doctor = doctor;      
+            var treatmentHistory = new TreatmentHistory
+            {
+                MedicalProfileId = medicalProfileId,
+                OnSetDate = DateTime.Now,
+                DateCreated = DateTime.Now
+            };
+            return PartialView("Create", treatmentHistory);
+        }
+
+        [HttpPost]
+        public JObject Create(TreatmentHistory treatmentHistory)
+        {
+            _db.TreatmentHistories.Add(treatmentHistory);
+            _db.SaveChanges();
+            dynamic result = new JObject();
+            result.result = "ok";
+            return result;
+        }
+    }
+}
--- trunk/OMCS/ApplicationWeb/Controllers/UserMedicalProfileController.cs
+++ trunk/OMCS/ApplicationWeb/Controllers/UserMedicalProfileController.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json.Linq;
 using OMCS.BLL;
 using OMCS.DAL.Model;
+using OMCS.Web.DTO;
 using Security.DAL.Security;
 using System;
 using System.Collections.Generic;
@@ -26,9 +27,24 @@ namespace OMCS.Web.Controllers
 
         public ActionResult Index()
         {
-            var medicalProfiles = _db.MedicalProfiles.Where(a => a.PatientId == User.UserId).ToList();
-            ViewBag.medicalProfiles = medicalProfiles;
-            return View(medicalProfiles);
+            var patient = _db.Patients.Find(User.UserId);
+            var medicalProfiles = _db.MedicalProfiles.Where(mp => mp.PatientId == User.UserId).ToList();
+            var medicalProfileTemplates = _db.MedicalProfileTemplates.ToList();
+            SelectList medicalProfileTemplateList = new SelectList(medicalProfileTemplates,
+                "MedicalProfileTemplateId", "MedicalProfileTemplateName",
+                medicalProfileTemplates.First());
+            var doctorName = _db.Doctors.ToList();
+            SelectList doctorNameList = new SelectList(doctorName,
+                "UserId", "FullName",
+                doctorName.First());
+            ViewBag.doctorNameList = doctorNameList;
+            PatientInformation PatientInformation = new PatientInformation
+            {
+                Patient = patient,
+                MedicalProfiles = medicalProfiles
+            };
+            ViewBag.medicalProfileTemplateList = medicalProfileTemplateList;
+            return PartialView("Index", PatientInformation);
         }
 
         public ActionResult Details(int medicalProfileId)
@@ -42,6 +58,50 @@ namespace OMCS.Web.Controllers
             ViewBag.createdDate = medicalProfile.CreatedDate;
             ViewBag.hospital = _db.HospitalInformations.First().Name;
             return View();
+        }
+
+        public ActionResult UpdateMedicalProfile(int id = 0, int medicalProfileTemplateId = 0, int doctorId = 0, int medicalProfileId = 0)
+        {
+            MedicalProfile medicalProfile;          
+            //Get Medical Profile by medicalProfileId
+            if (medicalProfileId != 0)
+            {
+                medicalProfile = _db.MedicalProfiles.Find(medicalProfileId);
+                ViewBag.formInJson = business.UpdateMedicalProfile(
+                    medicalProfile.PatientId, medicalProfile.MedicalProfileTemplateId, doctorId);
+            }
+            else
+            {
+                //Get Medical Profile by patientId and medicalProfileTemplateId
+                medicalProfile = _db.MedicalProfiles.
+                    Where(x => x.MedicalProfileTemplateId == medicalProfileTemplateId
+                    && x.PatientId == id).FirstOrDefault();
+                ViewBag.formInJson = business.UpdateMedicalProfile(id, medicalProfileTemplateId, doctorId);
+                if (medicalProfile == null)
+                {
+                    medicalProfile = _db.MedicalProfiles.
+                    Where(x => x.MedicalProfileTemplateId == medicalProfileTemplateId
+                    && x.PatientId == id).FirstOrDefault();
+                }
+            }
+            ViewBag.patientId = medicalProfile.PatientId;
+            ViewBag.medicalProfileTemplateId = medicalProfile.MedicalProfileTemplateId;
+            ViewBag.medicalProfileName = _db.MedicalProfileTemplates.Find(medicalProfile.MedicalProfileTemplateId).MedicalProfileTemplateName;
+
+            ViewBag.medicalProfileId = medicalProfile.MedicalProfileId;
+            ViewBag.doctor = medicalProfile.Doctor;
+            ViewBag.createdDate = medicalProfile.CreatedDate;
+            ViewBag.hospital = _db.HospitalInformations.First().Name;
+            return View();
+        }
+
+        [HttpPost]
+        public JObject UpdateMedicalProfile(FormCollection formCollection)
+        {
+            business.UpdateMedicalProfileForPatient(formCollection);
+            dynamic result = new JObject();
+            result.result = "success";
+            return result;
         }
     }
 }
